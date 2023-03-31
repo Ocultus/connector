16fac8c532a67fcea458e5d404861b303ff1a135
Ocultus <shelgunovdima3@gmail.com>
2023-03-31 17:17:27 +0600

    feat: init fastify with trpc server on core

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -1,29 +1,28 @@
+import fastify from 'fastify';
 import {Bootstrap} from '../common/bootstap.module';
-import {configModule} from '../common/config/config.module';
-import {EmployeeRepository} from '../common/repository/employee.repository';
-import {EmployeeAlreadyExistException} from '../common/types/exceptions/user-exception.type';
+import {fastifyTRPCPlugin} from '@trpc/server/adapters/fastify';
+import {createContext} from './trpc/context';
+import { appRouter } from './trpc/_app';
 
 export class ApplicationModule {
-	private bootstrap: Bootstrap;
+	readonly bootstrap: Bootstrap;
 	constructor() {
 		this.bootstrap = new Bootstrap();
 	}
 
 	public init = async () => {
-		const databaseConfig = configModule.getDatabaseConfig();
-		const pgClient = await this.bootstrap.initDatabase(databaseConfig);
 		console.debug('database initialize finished');
 
-		const employeeRepository = new EmployeeRepository(pgClient);
-		//test
-		try {
-			await employeeRepository.insertOne('test', 'test', 'test');
-		} catch (e) {
-			if (e instanceof EmployeeAlreadyExistException) {
-				console.log(e.message);
-			}
-		}
-		console.log(await employeeRepository.findOneByCredentials('test', 'test'));
+		const server = fastify({
+			maxParamLength: 5000,
+		});
+
+		server.register(fastifyTRPCPlugin, {
+			prefix: 'api/trpc',
+			trpcOptions: {router: appRouter, createContext},
+		});
+
+		await server.listen({port: 3000});
 	};
 }
 
