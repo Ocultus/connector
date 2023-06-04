45a4ec2674f648321eda01ab47dfd2bd808b4cff
Ocultus <shelgunovdima3@gmail.com>
2023-06-04 16:41:48 +0600

    feat: update migrations

--- migrations/init.sql
+++ migrations/init.sql
@@ -6,64 +6,63 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
-CREATE TABLE customer(
+CREATE TABLE IF NOT EXISTS customer(
     id SERIAL CONSTRAINT "customer_pk" PRIMARY KEY,
     name VARCHAR,
     email VARCHAR NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE UNIQUE INDEX "customer_email_idx" ON customer USING btree (email);
+CREATE UNIQUE INDEX IF NOT EXISTS "customer_email_idx" ON customer USING btree (email);
 
 CREATE TYPE gateway_type AS ENUM ('tg','vk');
 
-CREATE TABLE gateway (
+CREATE TABLE IF NOT EXISTS gateway (
     id SERIAL CONSTRAINT "gateway_pk" PRIMARY KEY,
     name VARCHAR NOT NULL,
     customer_id SERIAL NOT NULL REFERENCES customer(id),
     credentials jsonb NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     type gateway_type NOT NULL,
-    enabled boolean NOT NULL DEFAULT true
+    enabled boolean NOT NULL DEFAULT true,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE client (
+CREATE TABLE IF NOT EXISTS client (
     id SERIAL CONSTRAINT "client_pk" PRIMARY KEY,
     external_id SERIAL NOT NULL,
     name VARCHAR NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
-CREATE UNIQUE INDEX "client_external_id_idx" ON client USING btree (external_id);
+CREATE UNIQUE INDEX IF NOT EXISTS "client_external_id_idx" ON client USING btree (external_id);
 
-CREATE TABLE chat (
+CREATE TABLE IF NOT EXISTS chat (
     id SERIAL CONSTRAINT "chat_pk" PRIMARY KEY,
     client_id SERIAL NOT NULL CONSTRAINT client_id_fk REFERENCES client ON UPDATE cascade ON DELETE CASCADE,
-    gateway_id SERIAL NOT NULL CONSTRAINT gateway_id_fk REFERENCES gateway ON UPDATE cascade ON DELETE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
-CREATE UNIQUE INDEX "chat_idx" ON chat USING btree (client_id,gateway_id);
-
 CREATE TYPE message_type_enum as ENUM ('incoming','outgoing');
 
-CREATE TABLE message (
+CREATE TABLE IF NOT EXISTS message (
     id SERIAL CONSTRAINT "message_pk" PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL DEFAULT now(),
     parent_id SERIAL REFERENCES message(id),
     chat_id SERIAL REFERENCES chat(id),
     payload jsonb NOT NULL,
-    type message_type_enum NOT NULL
+    type message_type_enum NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TYPE request_type_enum as ENUM ('open','pending','closed');
 
-CREATE TABLE request (
+CREATE TABLE IF NOT EXISTS request (
     id SERIAL CONSTRAINT "request_pk" PRIMARY KEY,
+    gateway_id SERIAL NOT NULL CONSTRAINT request_getaway_id REFERENCES gateway ON UPDATE CASCADE ON DELETE CASCADE,
     chat_id SERIAL NOT NULL CONSTRAINT chat_id_fk REFERENCES chat ON UPDATE CASCADE ON DELETE CASCADE,
     type request_type_enum NOT NULL DEFAULT 'open',
-    created_at TIMESTAMP NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP
+    updated_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TRIGGER refresh_updated_at_column_request
